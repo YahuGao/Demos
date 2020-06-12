77d70d0f7b8905d0aee480bb1547f0a4776a44c7
Yahu Gao <yahu.gao@outlook.com>
2020-06-12 11:06:43 -0400

    emacs: Configure for grip mode
    Render markdown files with GitHub markdown.
    
    Signed-off-by: Yahu Gao <yahu.gao@outlook.com>

--- emacs/emacs.d/lisp/init-packages.el
+++ emacs/emacs.d/lisp/init-packages.el
@@ -12,6 +12,8 @@
 	smartparens
 	;; --- Themes ---
 	monokai-theme
+        markdown-mode
+        grip-mode
 	;; solarized-theme
 	) "Default packages")
 
@@ -31,5 +33,27 @@
 
 ;; load theme
 (load-theme 'monokai 1)
+(require 'markdown-mode)
+;; -----------------------------Configuration of grip-mode-------------------------------------
+;; Make a keybinding: `C-c C-c g'
+(define-key markdown-mode-command-map (kbd "g") #'grip-mode)
+;; Or start grip when opening a markdown/org buffer
+(add-hook 'markdown-mode-hook #'grip-mode)
+(add-hook 'org-mode-hook #'grip-mode)
+;; Path to the grip binary
+(setq grip-binary-path "/usr/bin/grip")
+;; A GitHub username for API authentication
+(setq grip-github-user "YahuGao")
+;; A GitHub password or auth token for API auth
+(setq grip-github-password "c08aea33e429699054142cf85bf8e0c0bc63bbb8")
+;; When nil, update the preview after file saves only, instead of also
+;; after every text change
+(setq grip-update-after-change nil)
+;; Use embedded webkit to previe
+;; This requires GNU/Emacs version >= 26 and built with the `--with-xwidgets`
+;; option.
+(setq grip-preview-use-webkit t)
+;; -----------------------------Configuration of grip-mode-------------------------------------
+
 ;; 文件末尾
 (provide 'init-packages)
