180a7e3f77010df62bdf968dd6e4909ed9f2e0a8
Yahu Gao <yahu.gao@outlook.com>
2020-06-19 17:57:25 -0400

    emacs: Add package emacs-jupyter
    Signed-off-by: Yahu Gao <yahu.gao@outlook.com>

--- emacs/emacs.d/lisp/init-packages.el
+++ emacs/emacs.d/lisp/init-packages.el
@@ -10,7 +10,7 @@ which is unsafe because it allows man-in-the-middle attacks.
 There are two things you can do about this warning:
 1. Install an Emacs version that does support SSL and be safe.
 2. Remove this warning from your init file so you won't see it again."))
-  ;; (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
   ;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
   ;; and `package-pinned-packages`. Most users will not need or want to do this.
   (add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
@@ -37,6 +37,7 @@ There are two things you can do about this warning:
 	elpy                    ;; Emacs Lisp Python Environment
 	flycheck                ;; On the fly syntax checking
 	py-autopep8             ;; Run autopep8 on save
+	jupyter
 	magit                   ;; Git integration
 	auctex                  ;; Write and formate tex files
 	reftex                  ;; Help inserting labels, references and citations of tex
