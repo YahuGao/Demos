af92573915e6df72af765c7e8bc931d3c55b95d6
Yahu Gao <yahu.gao@outlook.com>
2020-06-24 10:24:45 -0400

    emacs: Try to configure htlatex
    Unusable now.
    
    Signed-off-by: Yahu Gao <yahu.gao@outlook.com>

--- emacs/emacs.d/lisp/config-tex.el
+++ emacs/emacs.d/lisp/config-tex.el
@@ -3,6 +3,7 @@
 (add-hook 'LaTeX-mode-hook
 	  (lambda ()
 	    (add-to-list 'TeX-command-list '("XeLaTeX" "%`xelatex%(mode)%' %t" TeX-run-TeX nil t))
+	    (add-to-list 'TeX-command-list '("HtLaTex" "%`htlatex%(mode)%' %t" TeX-run-TeX nil t))
 	    (setq TeX-command-default "XeLaTeX")))
 
 (defun my-LaTeX-mode()
