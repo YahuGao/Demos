fa6a40c379abc66e8ab781a9badaa051d24fad38
Yahu Gao <yahu.gao@outlook.com>
2020-07-17 21:49:41 -0400

    Revert "emacs: Stop configuring again and again"
    This reverts commit 15131e6826a8b56c14bcc41115862fc2ce3b2543.

---
+++ emacs/emacs.d/lisp/config-packages.el
@@ -0,0 +1,10 @@
+(require 'config-theme)
+(require 'config-markdown)
+
+;; >>> Configuration of python IDE >>>
+(require 'config-python)
+;; <<< Configuration of python IDE <<<
+;; >>> configuration of TEX >>>
+(require 'config-tex)
+;; <<< configuration of TEX <<<
+(provide 'config-packages)
