f9b1c1c17d0dd05c495d652578d88ca3e3951705
Yahu Gao <yahu.gao@outlook.com>
2020-07-18 20:07:29 -0400

    emacs: Fix bug file read only
    Signed-off-by: Yahu Gao <yahu.gao@outlook.com>

--- emacs/emacs.d/lisp/config-jupyternb.el
+++ emacs/emacs.d/lisp/config-jupyternb.el
@@ -1,2 +1,6 @@
 (require 'ein)
+(add-hook 'before-save-hook
+          (lambda ()
+            (when (not (derived-mode-p 'ein:notebook-multilang-mode))
+              (delete-trailing-whitespace))))
 (provide 'config-jupyternb)
