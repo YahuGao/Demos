5e4e2de569ec0861af3b8b659f88a226c989d228
Yahu Gao <yahu.gao@outlook.com>
2020-08-02 01:21:33 +0900

    emacs: Enable show images in notebook
    Signed-off-by: Yahu Gao <yahu.gao@outlook.com>

--- emacs/emacs.d/init.el
+++ emacs/emacs.d/init.el
@@ -54,3 +54,16 @@
 (tool-bar-mode 0)
 (menu-bar-mode 0)
 (scroll-bar-mode 0)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(conda-anaconda-home "/home/yahu/anaconda3/")
+ '(ein:output-area-inlined-images t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
